94bfdf46d1a3dea2ab0da1a9b22448e9702ebc6f
skojphy <skojphy@gmail.com>
2021-09-22 12:37:35 +0900

    style: Remove console.log codes
    It was just for tests.

--- isEqual&deepClone/deepClone.js
+++ isEqual&deepClone/deepClone.js
@@ -5,19 +5,16 @@ const deepClone = object => {
 
   if (object === null || typeof object !== 'object') {
     cloned = object;
-    console.log('A', cloned, typeof cloned);
   } else if (Array.isArray(object)) {
     object.forEach((element, index) => {
       clonedArray[index] = deepClone(element);
     });
     cloned = clonedArray;
-    console.log('B', cloned, typeof cloned);
   } else {
     Object.entries(object).forEach(element => {
       clonedObject[element[0]] = deepClone(element[1]);
     });
     cloned = clonedObject;
-    console.log('C', cloned, typeof cloned);
   }
 
   return cloned;
